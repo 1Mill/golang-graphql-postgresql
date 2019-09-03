db529b9b8bc59431b3b8210ab7c4ba8b041e78fb
1Mill <erik.ekberg@outlook.com>
2019-09-02 20:21:07 -0700

    Update resolvers.go

--- src/backend/cmd/resolvers.go
+++ src/backend/cmd/resolvers.go
@@ -3,10 +3,12 @@ package main
 // Query placeholder
 type Query struct{}
 
+// Hello resolves hello query
 func (_ *Query) Hello() string {
 	return "Hello, World!"
 }
 
+// Testing resolves testing query
 func (_ *Query) Testing() string {
 	return "SLKFJLKSDJFLJDSLKFSF"
 }
